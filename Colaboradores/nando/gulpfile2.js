dbbf4139909dd94513ebd81a4852a6dd9ec426c5
NandoAlonso <nando7211@gmail.com>
2016-09-29 12:02:34 +0200

    asd

---
+++ Colaboradores/nando/gulpfile2.js
@@ -0,0 +1,10 @@
+
+var gulp = require('gulp');
+
+var destino ="C:/Users/elUsuario/Desktop/despliegues";
+var srcHTML="./src/html/*.html";
+
+gulp.task("default", function() {
+    gulp.src(srcHTML)
+        .pipe(gulp.dest(destino));
+    });
